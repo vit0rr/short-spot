27a71ad389bda2e9ffef786b98f8cbb0c9689820
vitor <vi.souza.almeida@protonmail.com>
2024-10-17 12:55:25 -0300

    refactor(database): improve database client reference (#3)

--- api/server/server.go
+++ api/server/server.go
@@ -7,10 +7,11 @@ import (
 
 	"github.com/vit0rr/short-spot/api/router"
 	"github.com/vit0rr/short-spot/pkg/deps"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func New(ctx context.Context, deps *deps.Deps) *http.Server {
-	router := router.New(deps)
+func New(ctx context.Context, deps *deps.Deps, db *mongo.Database) *http.Server {
+	router := router.New(deps, *db)
 	timeout := time.Duration(deps.Config.Server.CtxTimeout) * time.Second
 
 	return &http.Server{

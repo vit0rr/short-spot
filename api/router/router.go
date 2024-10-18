8214c70ca23f80f4730eec978ffcdc1fd3b12cbd
vitor <vi.souza.almeida@protonmail.com>
2024-10-17 22:31:37 -0300

    feat(redirect): remove old user setup

--- api/router/router.go
+++ api/router/router.go
@@ -4,7 +4,6 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	urlshort "github.com/vit0rr/short-spot/api/internal/url-short"
-	"github.com/vit0rr/short-spot/api/internal/users"
 	"github.com/vit0rr/short-spot/pkg/deps"
 	"github.com/vit0rr/short-spot/pkg/telemetry"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -12,14 +11,12 @@ import (
 
 type Router struct {
 	Deps     *deps.Deps
-	users    *users.HTTP
 	urlshort *urlshort.HTTP
 }
 
 func New(deps *deps.Deps, db mongo.Database) *Router {
 	return &Router{
 		Deps:     deps,
-		users:    users.NewHTTP(deps, &db),
 		urlshort: urlshort.NewHTTP(deps, &db),
 	}
 }
@@ -33,14 +30,11 @@ func (router *Router) BuildRoutes() *chi.Mux {
 	r.Use(telemetry.TelemetryMiddleware)
 
 	r.Route("/", func(r chi.Router) {
-		r.Route("/users", func(r chi.Router) {
-			r.Get("/", telemetry.HandleFuncLogger(router.users.List))
-			r.Post("/create", telemetry.HandleFuncLogger(router.users.Create))
-		})
+		r.Get("/{id}", telemetry.HandleFuncLogger(router.urlshort.Redirect))
 		r.Route("/short-url", func(r chi.Router) {
 			r.Post("/", telemetry.HandleFuncLogger(router.urlshort.ShortUrl))
 		})
-		r.Get("/{id}", telemetry.HandleFuncLogger(router.urlshort.Redirect))
+
 	})
 
 	return r

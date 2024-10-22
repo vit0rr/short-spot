0d86c77931d34d68cd276594f9754609e4e51598
vitor <vi.souza.almeida@protonmail.com>
2024-10-21 21:49:47 -0300

    feat(middleware): add auth token middleware (#11)
    * feat(middleware): add auth token middleware
    
    * feat(middleware): do not check if get

--- api/router/router.go
+++ api/router/router.go
@@ -2,11 +2,14 @@ package router
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
 	urlshort "github.com/vit0rr/short-spot/api/internal/url-short"
 	"github.com/vit0rr/short-spot/pkg/deps"
+	"github.com/vit0rr/short-spot/pkg/log"
 	"github.com/vit0rr/short-spot/pkg/telemetry"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -14,6 +17,29 @@ import (
 type Router struct {
 	Deps     *deps.Deps
 	urlshort *urlshort.HTTP
+}
+
+func (router *Router) BuildRoutes() *chi.Mux {
+	r := chi.NewRouter()
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.StripSlashes)
+	r.Use(SetResponseTypeToJSON)
+	r.Use(telemetry.TelemetryMiddleware)
+
+	// Custom middleware
+	r.Use(corsMiddleware)
+	r.Use(authMiddleware)
+
+	r.Route("/", func(r chi.Router) {
+		r.Get("/{id}", telemetry.HandleFuncLogger(router.urlshort.Redirect))
+		r.Route("/short-url", func(r chi.Router) {
+			r.Post("/", telemetry.HandleFuncLogger(router.urlshort.ShortUrl))
+		})
+
+	})
+
+	return r
 }
 
 func New(deps *deps.Deps, db mongo.Database) *Router {
@@ -27,7 +53,7 @@ func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
@@ -38,23 +64,22 @@ func corsMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-func (router *Router) BuildRoutes() *chi.Mux {
-	r := chi.NewRouter()
-	r.Use(middleware.Recoverer)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.StripSlashes)
-	r.Use(SetResponseTypeToJSON)
-	r.Use(telemetry.TelemetryMiddleware)
+func authMiddleware(next http.Handler) http.Handler {
+	godotenv.Load()
 
-	r.Use(corsMiddleware)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			next.ServeHTTP(w, r)
+			return
+		}
 
-	r.Route("/", func(r chi.Router) {
-		r.Get("/{id}", telemetry.HandleFuncLogger(router.urlshort.Redirect))
-		r.Route("/short-url", func(r chi.Router) {
-			r.Post("/", telemetry.HandleFuncLogger(router.urlshort.ShortUrl))
-		})
+		token := r.Header.Get("Authorization")
+		if token != os.Getenv("AUTH_TOKEN") {
+			log.Error(r.Context(), "Unauthorized. Please provide a valid auth token")
+			http.Error(w, "Unauthorized. Please provide a valid auth token", http.StatusUnauthorized)
+			return
+		}
 
+		next.ServeHTTP(w, r)
 	})
-
-	return r
 }

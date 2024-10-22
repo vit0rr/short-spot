8af603e9d71b8fe26d8dc5a83a351720bb73e5e2
vitor <vi.souza.almeida@protonmail.com>
2024-10-21 22:02:07 -0300

    refactor(middleware): move middlewares to middleware file

--- api/router/router.go
+++ api/router/router.go
@@ -1,15 +1,10 @@
 package router
 
 import (
-	"net/http"
-	"os"
-
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-	"github.com/joho/godotenv"
 	urlshort "github.com/vit0rr/short-spot/api/internal/url-short"
 	"github.com/vit0rr/short-spot/pkg/deps"
-	"github.com/vit0rr/short-spot/pkg/log"
 	"github.com/vit0rr/short-spot/pkg/telemetry"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -24,12 +19,12 @@ func (router *Router) BuildRoutes() *chi.Mux {
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.RealIP)
 	r.Use(middleware.StripSlashes)
-	r.Use(SetResponseTypeToJSON)
-	r.Use(telemetry.TelemetryMiddleware)
 
-	// Custom middleware
-	r.Use(corsMiddleware)
-	r.Use(authMiddleware)
+	// Custom middlewares
+	r.Use(telemetry.TelemetryMiddleware)
+	r.Use(SetResponseTypeToJSON)
+	r.Use(CorsMiddleware)
+	r.Use(AuthMiddleware)
 
 	r.Route("/", func(r chi.Router) {
 		r.Get("/{id}", telemetry.HandleFuncLogger(router.urlshort.Redirect))
@@ -47,39 +42,4 @@ func New(deps *deps.Deps, db mongo.Database) *Router {
 		Deps:     deps,
 		urlshort: urlshort.NewHTTP(deps, &db),
 	}
-}
-
-func corsMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "POST")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
-
-		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusOK)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
-}
-
-func authMiddleware(next http.Handler) http.Handler {
-	godotenv.Load()
-
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.Method == http.MethodGet {
-			next.ServeHTTP(w, r)
-			return
-		}
-
-		token := r.Header.Get("Authorization")
-		if token != os.Getenv("AUTH_TOKEN") {
-			log.Error(r.Context(), "Unauthorized. Please provide a valid auth token")
-			http.Error(w, "Unauthorized. Please provide a valid auth token", http.StatusUnauthorized)
-			return
-		}
-
-		next.ServeHTTP(w, r)
-	})
 }

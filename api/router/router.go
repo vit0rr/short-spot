49624c645ed590d1f504e780f7acf290b17e0a84
vitor <vi.souza.almeida@protonmail.com>
2024-10-21 21:09:17 -0300

    feat(middleware): add auth token middleware

--- api/router/router.go
+++ api/router/router.go
@@ -2,9 +2,11 @@ package router
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
 	urlshort "github.com/vit0rr/short-spot/api/internal/url-short"
 	"github.com/vit0rr/short-spot/pkg/deps"
 	"github.com/vit0rr/short-spot/pkg/telemetry"
@@ -14,6 +16,29 @@ import (
 type Router struct {
 	Deps     *deps.Deps
 	urlshort *urlshort.HTTP
+}
+
+func (router *Router) BuildRoutes() *chi.Mux {
+	r := chi.NewRouter()
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.StripSlashes)
+	r.Use(SetResponseTypeToJSON)
+	r.Use(telemetry.TelemetryMiddleware)
+
+	// Custom middleware
+	r.Use(corsMiddleware)
+	r.Use(authMiddleware)
+
+	r.Route("/", func(r chi.Router) {
+		r.Get("/{id}", telemetry.HandleFuncLogger(router.urlshort.Redirect))
+		r.Route("/short-url", func(r chi.Router) {
+			r.Post("/", telemetry.HandleFuncLogger(router.urlshort.ShortUrl))
+		})
+
+	})
+
+	return r
 }
 
 func New(deps *deps.Deps, db mongo.Database) *Router {
@@ -38,23 +63,16 @@ func corsMiddleware(next http.Handler) http.Handler {
 	})
 }
 
-func (router *Router) BuildRoutes() *chi.Mux {
-	r := chi.NewRouter()
-	r.Use(middleware.Recoverer)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.StripSlashes)
-	r.Use(SetResponseTypeToJSON)
-	r.Use(telemetry.TelemetryMiddleware)
+func authMiddleware(next http.Handler) http.Handler {
+	godotenv.Load()
 
-	r.Use(corsMiddleware)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
+		if token != os.Getenv("AUTH_TOKEN") {
+			http.Error(w, "Unauthorized. Please provide a valid auth token", http.StatusUnauthorized)
+			return
+		}
 
-	r.Route("/", func(r chi.Router) {
-		r.Get("/{id}", telemetry.HandleFuncLogger(router.urlshort.Redirect))
-		r.Route("/short-url", func(r chi.Router) {
-			r.Post("/", telemetry.HandleFuncLogger(router.urlshort.ShortUrl))
-		})
-
+		next.ServeHTTP(w, r)
 	})
-
-	return r
 }
